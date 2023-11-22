18bbcb3e04759d40fde8fef6846d6292f48aa4d7
aleksandraZhu <151791095+aleksandraZhu@users.noreply.github.com>
2023-11-22 23:16:14 +0100

    Update Main.java
    spel bord

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
+     static String board[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+    static void printBoard() {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0]  + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println("|---|---|---|");
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println("testing");
+        printBoard();
     }
 }
