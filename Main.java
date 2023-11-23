83945e139e01ebb58e7979324cbb625431188410
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-11-23 06:46:30 +0100

    La till knappar i en container

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 public static void main(String[] args) {
     JFrame frame = new JFrame("Tic Tac Toe");
     frame.setSize(600,500);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLayout(new BorderLayout());
     frame.setVisible(true);
+
+    //container för knappar ( för layout )
+    JPanel buttonContainer = new JPanel();
+    buttonContainer.setLayout(new GridLayout(3,3));
+    // skapar 9 knappar i en array
+    JButton[] gameButtons = new JButton[9];
+
+    // itererar genom listan och skriver ut 9 knappar till containern
+    for (int i = 0; i < 9; i++){
+    gameButtons[i] = new JButton("");
+    buttonContainer.add(gameButtons[i]);
+    }
+    frame.add(buttonContainer);
+
+
+
+
+
     }
 }
