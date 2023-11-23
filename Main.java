482fc04f31f45014d7f825c67b9492f58779fc5c
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-11-23 08:02:23 +0100

    Gjorde om knapparna till en array knappar istället så att man enklare kan implementera övrig logik

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 public class Main {
     private static Font universalFont = new Font(Font.SANS_SERIF, Font.BOLD, 50);
     private static boolean playerOne = true;
+
+
 public static void main(String[] args) {
     JFrame frame = new JFrame("Tic Tac Toe");
     frame.setSize(600,500);
@@ -19,42 +21,37 @@ public static void main(String[] args) {
     JPanel buttonContainer = new JPanel();
     buttonContainer.setLayout(new GridLayout(3,3));
 
-    // itererar genom listan och skriver ut 9 knappar till containern
-    for (int i = 0; i < 9; i++){
-        buttonContainer.add(createBoard());
+    // array av knappar
+    JButton[] gameButtons = new JButton[9];
+    // for loop som skapar knapparna och lägger in i buttonContainer
+    for(int i=0;i<9;i++){
+        gameButtons[i] = new JButton();
+        gameButtons[i].setFont(universalFont);
+        int finalI = i;
+
+        // ActionListener till knapparna
+        gameButtons[i].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // om playerOne = true och knappen inte är tagen kan de lägga sitt drag
+                if (playerOne & gameButtons[finalI].getText().isEmpty()){
+                    gameButtons[finalI].setText("X");
+                    // playOne = false ( alltså O's tur efter dreaget )
+                    playerOne = false;
+                }
+                // vice versa
+                else if (!playerOne & gameButtons[finalI].getText().isEmpty()){
+                    gameButtons[finalI].setText("O");
+                    playerOne = true;
+                }
+            }
+        });
+        buttonContainer.add(gameButtons[i]);
     }
+
     frame.add(buttonContainer);
-
-
-
     frame.setVisible(true);
     }
 
-
-    public static JButton createBoard(){
-    JButton gameButtons = new JButton();
-    gameButtons.setFont(universalFont);
-
-
-        gameButtons.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // om playerOne = true
-                if (playerOne & gameButtons.getText().isEmpty()){
-                    gameButtons.setText("X");
-                    // playOne = false ( alltså O's tur )
-                    playerOne = false;
-                }
-                else if (!playerOne & gameButtons.getText().isEmpty()){
-                    gameButtons.setText("O");
-                    playerOne = true;
-                }
-
-
-            }
-        });
-
-        return gameButtons;
-    }
 
 }
