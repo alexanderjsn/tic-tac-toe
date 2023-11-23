3006514934df65c0135d11e2daec085c062b3de6
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2023-11-23 06:38:09 +0100

    Skapade en JFrame

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 public class Main {
 public static void main(String[] args) {
     JFrame frame = new JFrame("Tic Tac Toe");
-    frame.setSize(500,400);
+    frame.setSize(600,500);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
     }
